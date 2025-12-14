31715938ac1201910222a359e690860518bb9d98
Suthakar Thiroshan <sthiroshan@gmail.com>
2025-12-14 20:29:11 +0530

    Fixed major bug in plates.py, Fixed detecting first two letters in plate number

--- plates.py
+++ plates.py
@@ -4,7 +4,6 @@
 # “No periods, spaces, or punctuation marks are allowed.”
 # Assume that any letters in the user’s input will be uppercase. 
 
-
 def main():
 
     plate = input("Plate: ")
@@ -22,49 +21,49 @@ def is_valid(s):
         return False
 
 
-def length_check(plate):
+def length_check(plate): 
 
     if 2 <= len(plate) <= 6: # Check whether plate is in correct range of length
         return True
     else:
         return False
 
-def first_2_letter_check(plate):
+def first_2_letter_check(plate): # THCXXX
 
     first_2_letter = plate[0:2] # Slice first two value of plate string
 
-    if not first_2_letter.isdecimal(): # Checks whether it's not numeric character 
-        return True # If it's not decimal, it's Alphabet ! (Note: punctuation check is checked in other function)
-    else:
-        return False 
+    if first_2_letter[0].isalpha() and first_2_letter[1].isalpha(): #Checks whether first and second char are letter
+        return True # if there is no number
+    
+    else: return False #If there is number
     
 def no_punctuation_space_check(plate):
 
     return plate.isalnum()
 
 def number_middle_check(plate):
-    # Itereate each element in that created list and check whether it contain number or not.
+    # Itereate each element in that string and check whether it contains number or not.
     # When iterating if there is number check remaining character are number or not
 
-    plate_list = list(plate) # Make new  list 
-    sliced_plate_list = plate_list[2:-1] # Slice it using [2:-1], it doesnt include first, second and last element. Because we dont need to check that whether it's number or not
+    sliced_plate = plate[2:-1] # Slice it using [2:-1], it doesnt include first, second and last element. Because we dont need to check that whether it's number or not
 
-    for char in sliced_plate_list: # Iterates for each element in the sliced list
+    for char in sliced_plate: # Iterates for each element in the sliced string
 
         if char.isdigit(): # Detects number
             # Note: return => Return value, break the loop and exit the function
                     
-            index_for_first_detected_number = plate_list.index(char) # Returns the index of the first occurrence of the detected number. 
-            remaining_list = plate_list[index_for_first_detected_number:]
-            remaining_string = "".join(remaining_list) # Joint the remaining list into string
+            index_for_first_detected_number = plate.index(char) # Returns the index of the first occurrence of the detected number. 
+            remaining_str = plate[index_for_first_detected_number:]
 
-            if char == "0": return False # Number starts with 0
+            if char == "0":  return False # Number starts with 0
             
-            if remaining_string.isdecimal(): #Return True or False by check if number isn't in the middle
+            if remaining_str.isdecimal(): #Return True or False by check if number isn't in the middle
                 return True # Number is not in the middle
             else: 
                 return False #Number is in the middle
 
     return True # Return True if there is no number in the middle 
 
-main()
+
+if __name__ == "__main__":
+    main()
